185102f9bcfd9f4758158cc920399d354eb61088
noxiss <kamiljakubczak@outlook.com>
2020-05-18 15:18:17 +0200

    Added basic functionality if creating movies based on external API

--- app/movie_api/serializers.py
+++ app/movie_api/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 
+from movie_api.models import Movie
 
-class MovieSerializer(serializers.Serializer):
+
+class TitleSerialzier(serializers.Serializer):
     """Serializer for movie objects"""
-
     title = serializers.CharField(max_length=255)
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    """Serializer for movie objects"""
+    Ratings = serializers.JSONField()
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
--- app/movie_api/views.py
+++ app/movie_api/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from movie_api import serializers
+from movie_api.models import Movie
 
 EXTERNAL_API_LINK = 'http://www.omdbapi.com'
 
@@ -14,16 +15,19 @@ EXTERNAL_API_LINK = 'http://www.omdbapi.com'
 class MovieApiView(APIView):
     """Movie API view"""
 
-    serializers_class = serializers.MovieSerializer
+    serializers_class = serializers.TitleSerialzier
+    movie_serializer_class = serializers.MovieSerializer
 
     def get(self, request, type=None):
         """Return items"""
-        return Response({'list': 'items'})
+        movies = Movie.objects.all()
+        serializer = self.movie_serializer_class(movies, many=True)
+
+        return Response(serializer.data)
 
     def post(self, request):
         """Create request for movie details and sate the result to db"""
         serializer = self.serializers_class(data=request.data)
-
         if serializer.is_valid():
             title = serializer.validated_data.get('title')
 
@@ -43,6 +47,14 @@ class MovieApiView(APIView):
                     {'error': movie_data.json()['Error']},
                     status=status.HTTP_400_BAD_REQUEST
                 )
+
+            movie_serializer = self.movie_serializer_class(
+                data=movie_data.json()
+            )
+
+            if movie_serializer.is_valid():
+                movie_serializer.save()
+                
             return Response({'details': movie_data.json()})
 
         else:
