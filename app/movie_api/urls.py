f97b118cf7ba719ed3c129d4d76607c94061e2c6
noxiss <kamiljakubczak@outlook.com>
2020-05-19 01:21:02 +0200

    Added fixes and added testing features

--- app/movie_api/urls.py
+++ app/movie_api/urls.py
@@ -8,7 +8,9 @@ app_name = 'api'
 urlpatterns = [
     path('movies/', views.MovieApiView.as_view(), name='movies'),
     path('movies/<int:pk>/', views.MovieApiView.as_view()),
-    path('comments/', views.CommentList.as_view(), name='comment'),
-    path('comments/<int:pk>/', views.CommentDetail.as_view()),
+    path('comments/', views.CommentList.as_view(), name='comments'),
+    path('comments/<int:pk>/',
+         views.CommentDetail.as_view(),
+         name='comment'),
     path('top/', views.Top.as_view(), name='top'),
 ]
